a60c89052b330b8372406ad809bfc85f2e2f3568
Matthew Wang <matt8p@outlook.com>
2020-12-15 16:00:54 -0700

    eventDate in contracts

--- functions/handlers/contracts.js
+++ functions/handlers/contracts.js
@@ -40,16 +40,16 @@ exports.createContract = (request, response) =>
     if(Object.keys(errors).length > 0)
         return response.status(500).json(errors); 
     
-    let clientDBPath = `/users/${newContract.clientHandle}`;
+    let clientDBPath = `/events/${newContract.eventID}`;
     db.doc(clientDBPath).get()                              //Check if the clientHandle and eventID match up 
     .then(doc =>
     {
         if(!doc.exists)
-            return response.status(500).json({clientHandle: 'Client user handle does not exist'});
-        let clientEvents = doc.data().events; 
-        if(!clientEvents.includes(newContract.eventID))
-            return response.status(500).json({eventID: `Invalid event ID for user ${newContract.clientHandle}`}); 
+            return response.status(500).json({eventID: `Event ${newContract.eventID} does not exist`});
+        if(doc.data().userHandle !== newContract.clientHandle)
+            return response.status(500).json({clientHandle: `User ${newContract.clientHandle} does not have event ${newContract.eventID}`});
         
+        newContract.eventDate = doc.data().eventDate;       //Insert eventDate into the database. 
         let contractID; 
         db.collection('contracts').add(newContract)
         .then(doc =>
@@ -59,11 +59,11 @@ exports.createContract = (request, response) =>
         })
         .then(() =>
         {
-            return response.status(201).json({message: `Contract ${contractID} successfully created`});
+            //TODO: Send a notification to the client that a contract has been created. 
         })
         .then(() =>
-        {
-            //TODO: Send a notification to the client that a contract has been created. 
+        {            
+            return response.status(201).json({message: `Contract ${contractID} successfully created`});
         })
     })
     .catch(err =>
@@ -144,6 +144,8 @@ exports.deleteContract = (request, response) =>
             return response.status(500).json({userHandle: `User ${userHandle} cannot delete contract ${contractID}`});
         if(!doc.data().active)
             return response.status(500).json({contract: `Contract ${contractID} already inactive`});
+        if(new Date().toISOString() > doc.data().eventDate)
+            return response.status(500).json({contract: `Cannot delete contract after event has occurred`});
         return db.doc(`/contracts/${contractID}`).update({active: false})
     })
     .then(() =>
@@ -151,16 +153,6 @@ exports.deleteContract = (request, response) =>
         db.doc(`/events/${eventID}`).get()
         .then(doc =>
         {
-            let eventDate = doc.data().eventDate                                        
-            if(new Date().toISOString() >= eventDate)                                                                   //Users cannot delete the contract after the event has occurred. 
-            {
-                db.doc(`/contracts/${contractID}`).update({active: true})
-                .then(() =>
-                {
-                    return response.status(500).json({contract: 'Contract cannot be deleted after the event date has occurred'});
-                })
-            }
-
             let services = doc.data().services; 
             services.forEach(serv =>
             {
