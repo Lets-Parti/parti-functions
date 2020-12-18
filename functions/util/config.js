b1c40fa3197a093929dc80f8648d3c5e5f360492
Matthew Wang <matt8p@outlook.com>
2020-12-18 13:10:23 -0700

    m

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,22 @@
-module.exports = {
-    apiKey: "AIzaSyAJvJxSfr3WrfxpHLVnEhfoPl_o3BdfRcM",
-    authDomain: "lets-parti.firebaseapp.com",
-    databaseURL: "https://lets-parti.firebaseio.com",
-    projectId: "lets-parti",
-    storageBucket: "lets-parti.appspot.com",
-    messagingSenderId: "922483274193",
-    appId: "1:922483274193:web:7576441a675d50bb8470f0",
-    measurementId: "G-17JK2V8HFB", 
-  };
+const parti_development = {
+  apiKey: "AIzaSyAJvJxSfr3WrfxpHLVnEhfoPl_o3BdfRcM",
+  authDomain: "lets-parti.firebaseapp.com",
+  databaseURL: "https://lets-parti.firebaseio.com",
+  projectId: "lets-parti",
+  storageBucket: "lets-parti.appspot.com",
+  messagingSenderId: "922483274193",
+  appId: "1:922483274193:web:7576441a675d50bb8470f0",
+  measurementId: "G-17JK2V8HFB", 
+};
+
+const parti_production = {
+  apiKey: "AIzaSyDLSFcazLn_MDtlilgasFsEFPKn13t14jc",
+  authDomain: "parti-production.firebaseapp.com",
+  projectId: "parti-production",
+  storageBucket: "parti-production.appspot.com",
+  messagingSenderId: "1052889994048",
+  appId: "1:1052889994048:web:5ec9265160f85fdf90e0a5",
+  measurementId: "G-74R0FZEWFC"
+}
+
+module.exports = parti_development
